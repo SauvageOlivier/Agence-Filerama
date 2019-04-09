685c52ae880a5dafebf713f1738f79c84d91ee46
Olivier Sauvage <Oliver0062@yahoo.fr>
2019-04-09 10:50:10 +0200

    injextion bdd->PropertyController

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 	
 	namespace App\Controller;
 	
+	use App\Entity\Property;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
@@ -17,9 +18,27 @@
 		 * @Route("/biens", name="property.index")
 		 * @return Response
 		 */
-		public function index() :Response
+		public function index() : Response
 		{
-			return $this->render('property/index.html.twig', [
+			$property = new Property();
+			$property
+				->setTitle('Mon premier bien')
+				->setPrice(200000)
+				->setRooms(5)
+				->setBedrooms(3)
+				->setDescription('Une petite description')
+				->setSurface(60)
+				->setFloor(4)
+				->setHeat(1)
+				->setCity('Montpellier')
+				->setAddress('23 Boulevard Voltaire')
+				->setPostalCode('34000');
+			
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($property);
+			$em->flush();
+			
+			return $this->render('property/index.html.twig.', [
 				'current_menu' => 'properties'
 			]);
 		}
