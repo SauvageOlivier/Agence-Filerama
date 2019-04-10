e26c28cab56fa81cf32c81d02933c4c79debd028
Olivier Sauvage <Oliver0062@yahoo.fr>
2019-04-10 12:12:15 +0200

    Form Ajouter

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -5,7 +5,9 @@
 	use App\Entity\Property;
 	use App\Form\PropertyType;
 	use App\Repository\PropertyRepository;
+	use Doctrine\Common\Persistence\ObjectManager;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\Routing\Annotation\Route;
 	
 	/**
@@ -18,35 +20,72 @@
 		 * @var PropertyRepository
 		 */
 		private $repository;
+		/**
+		 * @var ObjectManager
+		 */
+		private $em;
 		
 		/**
 		 * AdminPropertyController constructor.
 		 * @param PropertyRepository $repository
+		 * @param ObjectManager $em
 		 */
-		public function __construct( PropertyRepository $repository)
+		public function __construct( PropertyRepository $repository, ObjectManager $em )
 		{
 			$this->repository = $repository;
+			$this->em = $em;
 		}
 		
 		/**
-	 * @Route("/admin", name="admin.property.index")
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
+		 * @Route("/admin", name="admin.property.index")
+		 * @return \Symfony\Component\HttpFoundation\Response
+		 */
 		public function index()
 		{
 			$properties = $this->repository->findAll();
-			return $this->render('admin/property/index.hmtl.twig', compact('properties'));
+			return $this->render('admin/property/index.html.twig', compact('properties'));
 		}
 		
 		/**
-		 * @Route("/admin/{id}", name="admin.property.edit")
-		 * @param Property $property
+		 * @Route("/admin/property/create", name="admin.property.new")
+		 * @param Request $request
 		 * @return \Symfony\Component\HttpFoundation\Response
 		 */
-		public function edit(Property $property)
+		public function new(Request $request)
+		{
+			$property = new Property();
+			$form = $this->createForm(PropertyType::class, $property);
+			$form->handleRequest($request);
+			
+			if ( $form->isSubmitted() && $form->isValid() ) {
+				$this->em->persist($property);
+				$this->em->flush();
+				return $this->redirectToRoute('admin.property.index');
+			}
+			
+			return $this->render('admin/property/new.html.twig', [
+				'property' => $property,
+				'form' => $form->createView()
+			]);
+		}
+		
+		/**
+		 * @Route("/admin/property/{id}", name="admin.property.edit")
+		 * @param Property $property
+		 * @param Request $request
+		 * @return \Symfony\Component\HttpFoundation\Response
+		 */
+		public function edit( Property $property, Request $request )
 		{
 			$form = $this->createForm(PropertyType::class, $property);
-			return $this->render('admin/property/edit.hmtl.twig', [
+			$form->handleRequest($request);
+			
+			if ( $form->isSubmitted() && $form->isValid() ) {
+				$this->em->flush();
+				return $this->redirectToRoute('admin.property.index');
+			}
+			
+			return $this->render('admin/property/edit.html.twig', [
 				'property' => $property,
 				'form' => $form->createView()
 			]);
